15341f8d1eb61cfbf74ebdb2fc7241fd62aadf96
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2022-10-31 01:40:56 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## this project is to run simple django app on diffenent hosting enviroments  by using AWS RDS postgres database
+    Settings work with S3 bucket and AWS Postgress
---
+++ example/models.py
@@ -0,0 +1,8 @@
+
+import datetime
+from django.utils import timezone
+from django.db import models
+
+class Cat(models.Model):
+    name = models.CharField(max_length=30)
+    picture = models.FileField(upload_to='media/')
